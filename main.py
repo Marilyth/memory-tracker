842adfc0caea46130b7102c63f5d551f0a6d3d40
May <baermay98@gmail.com>
2023-02-14 13:35:58 +0100

    Allow searching for strings and addresses

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from mem_edit import Process
 import ctypes
 from typing import *
 import time
+import sys
 
 
-pid = input("Enter the process id or name to read, use 'ps -aux' to see options for names: ")
+pid = " ".join(sys.argv[1:])
 
 if not pid.isnumeric():
     process_name = pid
@@ -19,23 +20,85 @@ else:
 with Process.open_process(pid) as requested_process:
     addresses: List[int] = None
     while True:
-        current_value: int = int(input("Enter current value to search: "))
+        current_value = input("Enter current value to search: ")
 
-        addresses = requested_process.search_all_memory(ctypes.c_int(current_value)) if not addresses else\
-                    requested_process.search_addresses(addresses, ctypes.c_int(current_value))
+        if current_value.startswith("0x"):
+            print("Assuming memory")
+            addresses = [int(current_value.replace("s", ""), 16)]
+            current_value = (ctypes.c_char * 2).from_buffer(bytearray(2)) if "s" in current_value else ctypes.c_int()
+        else:
+            if current_value.isnumeric():
+                print("Assuming integer")
+                current_value = ctypes.c_int(int(current_value))
+            else:
+                print("Assuming string")
+                b = bytearray()
+                b.extend(current_value.encode("utf-8"))
+                current_value = (ctypes.c_char * len(b)).from_buffer(b)
+
+            addresses = requested_process.search_all_memory(current_value) if not addresses else\
+                        requested_process.search_addresses(addresses, current_value)
         
         address_expression = "\n".join([hex(address) for address in addresses][:100])
-        print(f"Found {len(addresses)} matches for value {current_value}:\n{address_expression}\n")
+        print(f"Found {len(addresses)} matches for value {current_value.value}:\n{address_expression}\n")
 
-        user_input = input("Enter 'r' to reset the current search, enter 't' to track the current addresses, anything else to continue filtering: ")
-        if user_input == "r":
-            addresses = None
-            current_value = print("Addresses were reset.")
-        if user_input == "t":
-            while True:
-                print("\nCurrent values:")
-                address_value = ctypes.c_int()
+        while True:
+            if len(addresses) == 0:
+                break
+
+            user_input = input("Enter 'r' to reset the current search, enter 'v' to show the value of the current addresses, enter 'a' to find addresses which point to the current matches, anything else to continue filtering: ")
+            print(user_input)
+            if user_input == "a":
+                pointer_addresses = set()
                 for address in addresses:
-                    requested_process.read_memory(address, address_value)
-                    print(f"{hex(address)}: {address_value.value}")
-                time.sleep(5)
+                    pointer_addresses.update(requested_process.search_all_memory(ctypes.c_int(address)))
+                addresses = pointer_addresses
+
+            elif user_input == "r":
+                addresses = []
+                print("Addresses were reset.")
+                break
+
+            elif user_input == "v":
+                # Allow reading bigger strings than the initial value.
+                if type(current_value).__name__.startswith("c_char_Array"):
+                    b = bytearray(100000)
+                    current_value = (ctypes.c_char * 100000).from_buffer(b)
+
+                # Avoid writing same values again, in case they are big.
+                string_occurrences = {}
+
+                print("\nCurrent values:")
+                for address in addresses:
+                    starting_address = address
+
+                    # Continue string to the left until null byte.
+                    if type(current_value).__name__.startswith("c_char_Array"):
+                        raw_value = (ctypes.c_byte * 1024)()
+                        
+                        while True:
+                            requested_process.read_memory(starting_address - 1024, raw_value)
+                            
+                            terminal_position = -1
+                            try:
+                                terminal_position = bytes(raw_value).rindex(0)
+                            except Exception:
+                                pass
+                            
+                            if terminal_position > -1:
+                                starting_address -= 1024 - terminal_position - 1
+                                break
+                            else:
+                                starting_address -= 1024
+
+                    requested_process.read_memory(starting_address, current_value)
+
+                    print()
+                    if current_value.value in string_occurrences:
+                        print(f"{hex(address)}: same as {hex(string_occurrences[current_value.value])}")
+                    else:
+                        print(f"{hex(address)}: {current_value.value}")
+                        string_occurrences[current_value.value] = address
+
+            else:
+                break
