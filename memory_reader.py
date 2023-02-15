85d54893a00fdda90f64dabb1e382bdacf2289d2
May <baermay98@gmail.com>
2023-02-15 11:10:43 +0100

    Add writing and conversion helper

--- memory_reader.py
+++ memory_reader.py
@@ -20,7 +20,26 @@ class MemoryReader:
         self.process: Process = Process(self.process_id)
         self.addresses = []
         self.buffer = None
-        
+    
+    @staticmethod
+    def _value_to_ctype(value: Union[int, str, float]):
+        """Converts the value to its ctype counterpart for searching in the memory.
+
+        Args:
+            value (_type_): The value to convert.
+
+        Returns:
+            _type_: A ctype object containing the value.
+        """
+        if type(value) == str:
+            b = bytearray()
+            b.extend(value.encode("utf-8"))
+            return (ctypes.c_char * len(b)).from_buffer(b)
+        elif type(value) == int:
+            return ctypes.c_int(value)
+        elif type(value) == float:
+            return ctypes.c_float(value)
+    
     def filter_value(self, value: Union[int, str, float], buffer = None) -> List[int]:
         """Search for the specified value in the currently filtered memory.
 
@@ -35,16 +54,8 @@ class MemoryReader:
         if buffer:
             self.buffer = buffer
         else:
-            if type(value) == str:
-                b = bytearray()
-                b.extend(value.encode("utf-8"))
-                self.buffer = (ctypes.c_char * len(b)).from_buffer(b)
-            elif type(value) == int:
-                self.buffer = ctypes.c_int(value)
-            elif type(value) == float:
-                self.buffer = ctypes.c_float(value)
+            self.buffer = MemoryReader._value_to_ctype(value)
         
-        print(self.buffer)
         if self.addresses:
             self.addresses = self.process.search_addresses(self.addresses, self.buffer)
         else:
@@ -100,3 +111,12 @@ class MemoryReader:
             values[address] = self.buffer.value
         
         return values
+    
+    def write_values(self, value: Union[int, str, float]) -> Dict[int, Union[int, str, float]]:
+        """Writes the value to all currently saved addresses.
+        """
+        value = MemoryReader._value_to_ctype(value)
+        
+        for address in self.addresses:
+            self.process.write_memory(address, value)
+            
